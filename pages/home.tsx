572cc966c2e2e97049c26fecd436a4b8dd1c8cf1
Zach Cossman <Zachcos@gmail.com>
2022-08-18 20:59:25 -0400

    Create home page

---
+++ pages/home.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import styled from 'styled-components';
+import { colors, font } from '../styles/variables';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>this is the home page</h1>
+    </div>
+  );
+};
+
+export default Home;
