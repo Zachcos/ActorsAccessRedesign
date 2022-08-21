654b3fed09a4362fd9abce2d441d523b6a4ad11b
Zach Cossman <Zachcos@gmail.com>
2022-08-21 14:19:24 -0400

    Add remaining icons to component

--- styles/icons.tsx
+++ styles/icons.tsx
@@ -99,4 +99,230 @@ const MailEmpty = () => (
   </svg>
 );
 
-export { Facebook, Instagram, Twitter, Apple, Arrow, Cart, MailEmpty };
+const Profile = () => (
+  <svg viewBox='0 0 14 16'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_47' data-name='Layer 47'>
+        <path
+          className='cls-1'
+          d='M12,0H2A2,2,0,0,0,0,2v9a2,2,0,0,0,2,2H4l3,3,3-3h2a2,2,0,0,0,2-2V2A2,2,0,0,0,12,0ZM7,1A2.5,2.5,0,1,1,4.5,3.5,2.5,2.5,0,0,1,7,1Zm4,10H3V9.37A2.37,2.37,0,0,1,5.37,7H8.63A2.37,2.37,0,0,1,11,9.37Z'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+const Resume = () => (
+  <svg viewBox='0 0 15 16'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_1-2' data-name='Layer 1'>
+        <path className='cls-1' d='M0,0V16H15V4.58L10.35,0ZM13,14H2V2H9V6h4Z' />
+        <rect className='cls-1' x='4' y='8' width='2' height='2' />
+        <rect className='cls-1' x='4' y='4' width='2' height='2' />
+        <rect className='cls-1' x='4' y='11' width='2' height='2' />
+        <rect className='cls-1' x='7' y='9' width='4' height='1' />
+        <rect className='cls-1' x='7' y='12' width='4' height='1' />
+      </g>
+    </g>
+  </svg>
+);
+
+const SizeCard = () => (
+  <svg viewBox='0 0 15 16'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_1-2' data-name='Layer 1'>
+        <polygon
+          className='cls-1'
+          points='6 9 6 8 4 8 4 7 6 7 6 6 4 6 4 5 6 5 6 4 4 4 2 4 2 3 3 3 4 3 6 3 6 2 4 2 4 1 6 1 6 0 4 0 2 0 1 0 0 0 0 1 0 4 0 5 0 9 4 9 6 9'
+        />
+        <polygon
+          className='cls-1'
+          points='14 10 14 12 13 12 13 10 12 10 12 12 12 14 11 14 11 12 11 10 10 10 10 12 9 12 9 10 8 10 8 12 7 12 7 10 6 10 6 12 6 14 5 14 5 12 5 10 4 10 4 12 3 12 3 10 2 10 2 12 1 12 1 10 0 10 0 12 0 14 0 16 15 16 15 14 15 12 15 10 14 10'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+const Media = () => (
+  <svg viewBox='0 0 15 16'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_1-2' data-name='Layer 1'>
+        <path
+          className='cls-1'
+          d='M12,9H0v5a2,2,0,0,0,2,2h9.17a2,2,0,0,0,1.42-.59l1.66-1.66A2.56,2.56,0,0,0,15,11.94V6ZM6,13H3V12H6Z'
+        />
+        <path className='cls-1' d='M10,8H1V6H9a1,1,0,0,1,1,1Z' />
+        <path
+          className='cls-1'
+          d='M5,0V2h7a1,1,0,0,1,1,1V6l1-1V1a1,1,0,0,0-1-1Z'
+        />
+        <path
+          className='cls-1'
+          d='M3,3V5h7a1,1,0,0,1,1,1V8l1-1V4a1,1,0,0,0-1-1Z'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+const Photo = () => (
+  <svg viewBox='0 0 16 16'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_1-2' data-name='Layer 1'>
+        <circle className='cls-1' cx='8' cy='7' r='2' />
+        <path
+          className='cls-1'
+          d='M13,14H3a2.8,2.8,0,0,1,2.05-2.68L6,11h4l.95.32A2.8,2.8,0,0,1,13,14Z'
+        />
+        <path
+          className='cls-1'
+          d='M0,0V16H16V0ZM6,1H7V2H6ZM4,1H5V2H4ZM2,1H3V2H2ZM14,14H2V3H14Z'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+const SlateShot = () => (
+  <svg viewBox='0 0 15 16'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_1-2' data-name='Layer 1'>
+        <rect className='cls-1' y='4' width='2' height='8' />
+        <rect className='cls-1' x='3' y='4' width='9' height='8' />
+        <rect className='cls-1' x='13' y='4' width='2' height='8' />
+        <path
+          className='cls-1'
+          d='M0,1V3H15V1a1,1,0,0,0-1-1H1A1,1,0,0,0,0,1ZM3,2H2V1H3ZM5,2H4V1H5ZM7,2H6V1H7ZM9,2H8V1H9Zm2,0H10V1h1Zm2,0H12V1h1Z'
+        />
+        <path
+          className='cls-1'
+          d='M0,13v2a1,1,0,0,0,1,1H14a1,1,0,0,0,1-1V13Zm3,2H2V14H3Zm2,0H4V14H5Zm2,0H6V14H7Zm2,0H8V14H9Zm2,0H10V14h1Zm2,0H12V14h1Z'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+const Gear = () => (
+  <svg viewBox='0 0 16 16'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_1-2' data-name='Layer 1'>
+        <path
+          className='cls-1'
+          d='M15,6H13.55c-.09,0-.19-.44-.32-.69l1-1a.81.81,0,0,0,0-1.17L12.84,1.74a.82.82,0,0,0-.6-.25.89.89,0,0,0-.6.25l-1,1c-.25-.13-.69-.23-.69-.32V1A1.13,1.13,0,0,0,9,0H7A1.13,1.13,0,0,0,6,1V2.45c0,.09-.44.19-.69.32l-1-1a.81.81,0,0,0-1.17,0L1.74,3.16a.84.84,0,0,0-.25.6.86.86,0,0,0,.25.6l1,.95c-.13.25-.23.69-.32.69H1A1.13,1.13,0,0,0,0,7V9a1.13,1.13,0,0,0,1,1H2.45c.09,0,.19.44.32.69l-1,1a.83.83,0,0,0,0,1.18l1.42,1.41a.86.86,0,0,0,1.2,0l.95-1c.25.13.69.23.69.32V15a1.13,1.13,0,0,0,1,1H9a1.13,1.13,0,0,0,1-1V13.55c0-.09.44-.19.69-.32l1,1a.81.81,0,0,0,1.17,0l1.41-1.42a.85.85,0,0,0,0-1.2l-1-1c.13-.25.23-.69.32-.69H15a1.13,1.13,0,0,0,1-1V7A1.13,1.13,0,0,0,15,6ZM8,10.92A2.92,2.92,0,1,1,10.92,8,2.92,2.92,0,0,1,8,10.92Z'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+const Representation = () => (
+  <svg viewBox='0 0 16 14'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_1-2' data-name='Layer 1'>
+        <circle className='cls-1' cx='4.5' cy='3.5' r='3.5' />
+        <circle className='cls-1' cx='11.5' cy='3.5' r='2.5' />
+        <path
+          className='cls-1'
+          d='M4,8H5a4,4,0,0,1,4,4v2a0,0,0,0,1,0,0H0a0,0,0,0,1,0,0V12A4,4,0,0,1,4,8Z'
+        />
+        <path
+          className='cls-1'
+          d='M16,13H8V11a4,4,0,0,1,4-4h0a4,4,0,0,1,4,4Z'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+const Contact = () => (
+  <svg viewBox='0 0 16 15'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_1-2' data-name='Layer 1'>
+        <path
+          className='cls-1'
+          d='M3,11H13a2,2,0,0,0,2-2V2a2,2,0,0,0-2-2H3A2,2,0,0,0,1,2V9A2,2,0,0,0,3,11ZM3,2H13V9H3Z'
+        />
+        <path
+          className='cls-1'
+          d='M0,12v2a1,1,0,0,0,1,1H15a1,1,0,0,0,1-1V12Zm10,2H6V13h4Z'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+const SignOut = () => (
+  <svg viewBox='0 0 16 16'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_1-2' data-name='Layer 1'>
+        <rect className='cls-1' x='7' width='2' height='6' />
+        <path
+          className='cls-1'
+          d='M11,.59V2.81a6,6,0,1,1-6,0V.59a8,8,0,1,0,6,0Z'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+const Warning = () => (
+  <svg viewBox='0 0 15.11 13.96'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_9' data-name='Layer 9'>
+        <path
+          className='cls-1'
+          d='M12.11,14h0l-9.1,0A3,3,0,0,1,.4,9.45h0L5,1.51A3,3,0,0,1,7.56,0h0a3,3,0,0,1,2.6,1.51l4.55,8A3,3,0,0,1,12.11,14Zm-10-3.52A1,1,0,0,0,3,11.94l9.1,0h0A1,1,0,0,0,13,10.47l-4.55-8A1,1,0,0,0,7.56,2a1,1,0,0,0-.87.5Z'
+        />
+        <path
+          className='cls-1'
+          d='M7.51,4h.1a.95.95,0,0,1,.95.95V7.47a.5.5,0,0,1-.5.5h-1a.5.5,0,0,1-.5-.5V4.92A.95.95,0,0,1,7.51,4Z'
+        />
+        <rect
+          className='cls-1'
+          x='6.56'
+          y='8.97'
+          width='2'
+          height='2'
+          rx='0.5'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+const CheckMark = () => (
+  <svg viewBox='0 0 14.78 13.7'>
+    <g id='Layer_2' data-name='Layer 2'>
+      <g id='Layer_72' data-name='Layer 72'>
+        <path
+          className='cls-1'
+          d='M2.64,6.05.09,8.6a.3.3,0,0,0,0,.45l5.5,4.58A.3.3,0,0,0,6,13.57L14.72,1.32a.3.3,0,0,0,0-.37L14,.11a.3.3,0,0,0-.46,0L5.86,8.84,3.07,6.05A.3.3,0,0,0,2.64,6.05Z'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export {
+  Facebook,
+  Instagram,
+  Twitter,
+  Apple,
+  Arrow,
+  Cart,
+  MailEmpty,
+  Profile,
+  Resume,
+  SizeCard,
+  Media,
+  Photo,
+  SlateShot,
+  Gear,
+  Representation,
+  Contact,
+  SignOut,
+  Warning,
+  CheckMark,
+};
