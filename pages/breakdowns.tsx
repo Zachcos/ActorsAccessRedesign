b871bfde79e016a1daa2fe67a0ae57784a71694a
Zach Cossman <Zachcos@gmail.com>
2022-08-22 18:57:00 -0400

    Add breakdowns page

---
+++ pages/breakdowns.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Breakdowns: NextPage = () => {
+  return (
+    <>
+      <h1>This is the breakdown page</h1>
+    </>
+  );
+};
+
+export default Breakdowns;
