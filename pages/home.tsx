2cfd47557731bf0f75622859961733fda0f33edb
Zach Cossman <Zachcos@gmail.com>
2022-08-22 14:12:05 -0400

    Reorganize home page layout

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,29 +1,34 @@
 import type { NextPage } from 'next';
+import styled from 'styled-components';
 import SideBar from '../components/sideBar';
 import UserCard from '../components/userCard';
 import ReminderCard from '../components/reminderCard';
 import UsBreakdowns from '../components/usBreakdowns';
 import YoutubePlaceholder from '../components/youtubePlaceholder';
 
+const HomeWrapper = styled.div`
+  display: flex;
+  margin-left: 180px;
+  padding: 24px 0 0 32px;
+  .one {
+    margin-right: 48px;
+  }
+`;
+
 const Home: NextPage = () => {
   return (
     <>
       <SideBar />
-      <div
-        style={{
-          marginLeft: '180px',
-          padding: '24px 0 0 32px',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'wrap',
-          maxHeight: '100vh',
-        }}
-      >
-        <UserCard />
-        <ReminderCard />
-        <UsBreakdowns />
-        <YoutubePlaceholder />
-      </div>
+      <HomeWrapper>
+        <div className='one'>
+          <UserCard />
+          <ReminderCard />
+        </div>
+        <div className='two'>
+          <UsBreakdowns />
+          <YoutubePlaceholder />
+        </div>
+      </HomeWrapper>
     </>
   );
 };
