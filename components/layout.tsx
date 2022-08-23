81b91509347198ffd12ffa45b568f3e1324c14bd
Zach Cossman <Zachcos@gmail.com>
2022-08-22 20:03:37 -0400

    Create layout component

---
+++ components/layout.tsx
@@ -0,0 +1,20 @@
+import SideBar from './sideBar';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-left: 180px;
+  padding: 24px 0 0 32px;
+`;
+
+interface ChildElementProp {
+  children: JSX.Element | JSX.Element[];
+}
+
+const Layout = ({ children }: ChildElementProp) => (
+  <>
+    <SideBar />
+    <Wrapper>{children}</Wrapper>
+  </>
+);
+
+export default Layout;
