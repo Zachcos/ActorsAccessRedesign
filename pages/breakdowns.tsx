6c66e8b51f0c460063521b1f8a9c831b732d648c
Zach Cossman <Zachcos@gmail.com>
2022-08-23 19:55:08 -0400

    Add Wrapper styled-component to center components on breakdowns page

--- pages/breakdowns.tsx
+++ pages/breakdowns.tsx
@@ -1,8 +1,22 @@
 import type { ReactElement } from 'react';
+import styled from 'styled-components';
 import Layout from '../components/layout';
+import SearchFilterCard from '../components/seachFilterCard';
+
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin-left: -32px;
+`;
 
 export default function Breakdowns() {
-  return <h1>This is the breakdowns component</h1>;
+  return (
+    <Wrapper>
+      <SearchFilterCard />
+      <h1 style={{ marginTop: '100px' }}>This will be a list of breakdowns</h1>
+    </Wrapper>
+  );
 }
 
 Breakdowns.getLayout = function getLayout(page: ReactElement) {
