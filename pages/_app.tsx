049fd307a859344e3b6650491bc01ec7088d629b
Zach Cossman <Zachcos@gmail.com>
2022-08-22 20:04:11 -0400

    Update _app to allow per-page layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import type { AppProps } from 'next/app';
+import type { NextPage } from 'next';
+import type { ReactElement, ReactNode } from 'react';
 import GlobalStyle from '../styles/globalStyles';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
     <>
       <GlobalStyle />
       <Component {...pageProps} />
