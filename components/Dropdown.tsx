51fa89431765b34481c64a0ec7986a918e5be297
Zach Cossman <Zachcos@gmail.com>
2022-09-06 09:13:29 -0400

    Add depthLevel variable and conditional render of dropdownClass for sub-submenu styling and spacing

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 import { colors, font } from '../styles/variables';
+import MenuItems from './MenuItems';
 
-const DropdownWrapper = styled.div`
+const Wrapper = styled.ul`
   background: ${colors.neut_2};
   border-radius: 8px;
   box-shadow: 0 4px 12px rgba(121, 130, 145, 0.4);
-  display: none;
-  left: 175px;
+  /* display: none; */
+  left: 170px;
+  right: 0;
+  top: -36px;
   padding: 24px 0;
   position: absolute;
   width: 200px;
@@ -20,29 +23,33 @@ const DropdownWrapper = styled.div`
     align-items: center;
     height: 34px;
     padding-left: 24px;
-    margin-bottom: 12px;
+    margin-bottom: 8px;
     text-align: left;
     &:hover {
       color: ${colors.neut_2};
       background: ${colors.neut_7};
     }
   }
+  .dropdown {
+    position: absolute;
+    li {
+      margin-bottom: 0;
+    }
+    &.dropdownSubmenu {
+      top: -24px;
+    }
+  }
 `;
 
-interface DropdownProps extends JSX.IntrinsicAttributes {
-  submenu: MenuItemProps[] | undefined;
-  show: boolean;
-}
-
-//! - Check if bang is needed after submenu
-
-const Dropdown = ({ submenu, show }: DropdownProps) => {
+const Dropdown = ({ submenu, depthLevel }: any) => {
+  depthLevel = depthLevel + 1;
+  const dropdownClass = depthLevel > 1 ? 'dropdownSubmenu' : '';
   return (
-    <DropdownWrapper className={show ? 'show' : ''}>
-      {submenu!.map((item, index) => {
-        return <li key={index}>{item.title}</li>;
-      })}
-    </DropdownWrapper>
+    <Wrapper className={`dropdown ${dropdownClass}`}>
+      {submenu.map((submenu: any, index: number) => (
+        <MenuItems items={submenu} key={index} depthLevel={depthLevel} />
+      ))}
+    </Wrapper>
   );
 };
 
