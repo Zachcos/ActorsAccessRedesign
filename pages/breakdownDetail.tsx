4d23986eb5e7b1eaa524dbdf3896efd9e1d123ec
Zach Cossman <Zachcos@gmail.com>
2022-09-09 17:28:36 -0400

    Add AlertCard copy and styles

--- pages/breakdownDetail.tsx
+++ pages/breakdownDetail.tsx
@@ -1,12 +1,37 @@
 import { ReactElement } from 'react';
+import styled from 'styled-components';
+import { colors, font } from '../styles/variables';
 import Layout from '../components/Layout';
 import AlertCard from '../components/AlertCard';
+
+const CardCopy = styled.div`
+  .title {
+    ${font(16, 800, `${colors.neut_8}`)};
+    margin-bottom: 24px;
+  }
+  .subtitle {
+    ${font(16, 500, `${colors.neut_6}`, 'italic')};
+    margin-bottom: 24px;
+  }
+  .copy {
+    ${font(14, 400, `${colors.neut_6}`)};
+    line-height: 1.05rem;
+  }
+`;
 
 const BreakdownDetail = () => {
   return (
     <>
-      <AlertCard type={'warn'} />
-      <h1>this is the breakdown details page</h1>
+      <AlertCard type={'warn'}>
+        <CardCopy>
+          <p className='title'>Submission Instructions</p>
+          <p className='subtitle'>Always submit ASAP!</p>
+          <p className='copy'>
+            Submitting within the first day of Breakdown publication is high
+            recommended
+          </p>
+        </CardCopy>
+      </AlertCard>
     </>
   );
 };
