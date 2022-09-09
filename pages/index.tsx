cf5d203611a7d27106677b0cc318f8bba8b6858f
Zach Cossman <Zachcos@gmail.com>
2022-09-09 17:28:03 -0400

    Refactor AlertCard copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { ReactElement } from 'react';
 import styled from 'styled-components';
+import { colors, font } from '../styles/variables';
 import UserCard from '../components/UserCard';
 import AlertCard from '../components/AlertCard';
 import UsBreakdowns from '../components/UsBreakdowns';
@@ -14,12 +15,45 @@ const HomeWrapper = styled.div`
   }
 `;
 
+const CardCopy = styled.div`
+  ${font(12, 400, `${colors.neut_6}`, 'italic')};
+  p {
+    line-height: 1.2rem;
+    margin-bottom: 1.8rem;
+    &:last-child {
+      margin-bottom: 0;
+    }
+    i {
+      ${font(12, 800)};
+    }
+    a {
+      color: ${colors.neut_6};
+    }
+  }
+`;
+
 const Home = () => {
   return (
     <HomeWrapper>
       <div className='one'>
         <UserCard />
-        <AlertCard type={'danger'} />
+        <AlertCard type={'danger'}>
+          <CardCopy>
+            <p>
+              Be sure to keep your profile and résumé up-to-date, and upload
+              your current photos so you and your representatives have access to
+              the most current and accurate information about you.
+            </p>
+            <p>
+              <i>Actors Access</i> uses your information to alert vou via email
+              when new Breakdowns on actors access match your profile. PLEASE
+              NOTE, if vou do not wish to receive information about certain
+              types of roles, or if vou do not wish to receive ANY
+              notifications, vou may update vour preferences on the{' '}
+              <a href='#'>My Account</a> page.
+            </p>
+          </CardCopy>
+        </AlertCard>
       </div>
       <div className='two'>
         <UsBreakdowns />
