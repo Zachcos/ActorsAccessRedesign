f0a236e585ce71efd06692cae62b749ec5b28b4b
Zach Cossman <Zachcos@gmail.com>
2022-09-09 22:36:27 -0400

    Create showData file for testing

---
+++ data/showData.ts
@@ -0,0 +1,100 @@
+const showData = [
+  {
+    gigInfo: {
+      title: 'A Wonderful World',
+      datePosted: 'Monday, Aug 1, 2022, 2:24pm Pacific',
+      venue: 'Pearl Studios',
+      union: 'AEA',
+      audDates: 'Immediate Virtual Submissions via Eco Cast',
+      audDeadline: 'August 11, 2022',
+      rehDates: 'September 6-20, 2022',
+      prodDates: 'September 21 - November 12, 2022',
+      prodPay: '$1,000/wk',
+      prodLocation: 'New York, NY',
+    },
+    creativeInfo: [
+      {
+        label: 'Producers',
+        value:
+          'Thomas E Rodgers Jr and Renee Delaplaine in association with Martian Entertainment',
+      },
+      {
+        label: 'Conceived By',
+        value: 'Christopher Renshaw, Andrew Delaplaine',
+      },
+      {
+        label: 'Director',
+        value: 'Christopher Renshaw',
+      },
+      {
+        label: 'Book',
+        value: 'Aurin Squire',
+      },
+      {
+        label: 'Choreographer',
+        value: 'Ricky Tripp',
+      },
+      {
+        label: 'Musical Director',
+        value: 'Annastasia Victory and Michael O Mitchell',
+      },
+      {
+        label: 'General Manager',
+        value: 'Carl D White, Martian Entertainment',
+      },
+      {
+        label: 'Associate Director',
+        value: 'Christina Sajous',
+      },
+      {
+        label: 'Associate Choreographer',
+        value: 'Aurelia Michael',
+      },
+      {
+        label: 'First Produced By',
+        value:
+          'Miami New Drama; Artistic Director Michel Hausmann, Managing Director Nicholas Richberg',
+      },
+      {
+        label: 'Casting Director',
+        value: 'Duncan Stewart CSA of StewartWhitley',
+      },
+      {
+        label: 'Casting Assistant',
+        value: 'Patrick Maravilla of StewartWhitley',
+      },
+    ],
+    roles: [
+      {
+        name: 'Louis Armstrong',
+        description: `30s-40s, male-identifying, Black. American Icon and International Music Legend. The father of Jazz. Born in the most
+        impoverished and violent part of New Orleans at the beginning of the 20th Century, his mother was a sex-worker, and
+        he never knew his father. He grew up hiding under the pianos in the brothels of Storyville. He wasn't a typically
+        "handsome" man. But had huge charisma, warmth and wit and lived only for his music and blowing his horn. His music
+        was the child he was always longing for.`,
+      },
+      {
+        name: 'Daisy Parker',
+        description: `20s, female-identifying, Black. Louis’ first wife. A knife-weilding sex-worker in Storyville, New Orleans. Fierce, earth, funny, and very sexual. On the edge. Her toughness and ciolence conceal her vulnerability.`,
+      },
+      {
+        name: 'Lil Hardin',
+        description: `25-30, female-idenfiying, Black. Louis’ second wife. College educated classical musican. Now a jazz pianist in Chicago. Way ahead of her time. Sharp, controlling, elegant. She transfers hwe own ambitions onto Louis, and grooms him into the icon the World came to know.`,
+      },
+      {
+        name: 'Alpha Smith',
+        description: `35, female-identifying, Black. Louis’ third wife. A former maid who dreams of luxury, lovely things, and family. Her dreams seem to be coming true swhen she moves with Louis to Hollywood. But when relegated by Louis to cooking and mending, the furs and jewels mean nothing and she aborts their baby and runs off with Louis’ drummer.`,
+      },
+      {
+        name: 'Lucille Wilson',
+        description: `40s, female-identifying, Black. Louis’ fourth wife. A former showgirl at the Cotton Club, who sees in Louis his real need for a ‘Home.’ A strong, centered, and magnificent woman who finally gives Louis a loving base. She nurtures him in the way he so badly needs but has never experienced.`,
+      },
+      {
+        name: `'King' Joe Oliver`,
+        description: `20s - 30s, male-identifying, Black. Legendary bandleading and trumpeter. Louis’ musical mentor and Father firgure. Macho and charismatic.`,
+      },
+    ],
+  },
+];
+
+export default showData;
