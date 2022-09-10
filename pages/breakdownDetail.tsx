e51e1cf40424382e9a729e983562a0afc01860c5
Zach Cossman <Zachcos@gmail.com>
2022-09-09 21:38:39 -0400

    Add fine print to breakdownDetail page

--- pages/breakdownDetail.tsx
+++ pages/breakdownDetail.tsx
@@ -19,9 +19,17 @@ const CardCopy = styled.div`
   }
 `;
 
+const FinePrint = styled.div`
+  ${font(12, 400, `${colors.neut_5}`)};
+  max-width: 400px;
+  line-height: 1.05rem;
+  margin-top: 8px;
+  margin-left: 48px;
+`;
+
 const BreakdownDetail = () => {
   return (
-    <>
+    <div style={{ display: 'flex' }}>
       <AlertCard type={'warn'}>
         <CardCopy>
           <p className='title'>Submission Instructions</p>
@@ -32,7 +40,18 @@ const BreakdownDetail = () => {
           </p>
         </CardCopy>
       </AlertCard>
-    </>
+      <FinePrint>
+        Casting information and descriptions listed below are the proprietary
+        property of Breakdown Services, Ltd. Descriptions created and or
+        published are confidential and copyright protected. No part of this
+        Breakdown may be posted on social media or published OR SHARED in any
+        manner without the prior and express written consent of Breakdown
+        Services, Ltd. Approved Talent Representatives are restricted to using
+        information contained in this Breakdown for the specific purpose of
+        submitting talent for available roles. Copyright © 2022 Breakdown
+        Services, Ltd. All Rights reserved.
+      </FinePrint>
+    </div>
   );
 };
 
